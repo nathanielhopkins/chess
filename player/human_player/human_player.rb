c0d4a3c6caddffbdf139e059e517ca7a6cb61e89
“NathanielHopkins” <nathanielchopkins@gmail.com>
2020-10-02 13:29:58 -0700

    rewrite human_player#make_move to only get_input from cursor

--- player/human_player/human_player.rb
+++ player/human_player/human_player.rb
@@ -1,17 +1,8 @@
 require_relative "../player.rb"
+
 class HumanPlayer < Player
 
   def make_move(board)
-    piece = @display.get_cursor_input
-    move = @display.get_cursor_input
-    if board[piece].color == @color
-      board.move_piece(piece,move) 
-    else
-      raise "Not your piece"
-    end
-    @display.render
-  rescue
-    puts "Not a valid move. Press Enter to continue."
-    retry
+    input = @display.get_cursor_input
   end
 end
